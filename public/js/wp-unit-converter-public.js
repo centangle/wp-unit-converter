306b34f1ca94dd14950fc372b18d9c2fdb548d5f
ihtishamzahoor <ihtisham.zahoor@outlook.com>
2019-02-04 19:43:51 +0500

    replaced server side calculation of units with js

--- public/js/wp-unit-converter-public.js
+++ public/js/wp-unit-converter-public.js
@@ -35,39 +35,115 @@
 
   $(document).ready(function($) {           
     $(document).on( "click", "#wpuc_convert", function() {
-      var from = $("#wpuc_from").val();
-      var to = $("#wpuc_to").val();
-      var wpuc_value = $("#wpuc_value").val();
-      var converter_type = $("#wpuc_converter_type").val();
 
-        $.post(wpuc_ajax_obj.ajax_url, {
-           _ajax_nonce: wpuc_ajax_obj.nonce,
-            action: "wpuc_do_convert",
-            from: from,
-            to: to,
-            wpuc_value: wpuc_value,
-            converter_type: converter_type
-        }, function(data) {
-          data = JSON.parse(data);
-          $("#wpuc_convert_result").html( "Result: " + data + " " + to );
-    		  $("#wpuc_convert_result").fadeIn();
-        });
+      $.ajax({
+        url: wpuc_ajax_obj.metrics_json,
+        type: "POST",
+        dataType: "json",
+        success: convertUnits,
+        error: function(xhr, status) {
+          console.log("There was an error. Request couldn't be completed.");
+        }
+      });
+
+      function convertUnits(data) {
+        var wpuc_from = $("#wpuc_from").val();
+        var wpuc_to = $("#wpuc_to").val();
+        var wpuc_value = $("#wpuc_value").val();
+        var wpuc_converter_type = $("#wpuc_converter_type").val();
+
+        var wpuc_metrics = data['metrics'];
+        var wpuc_converter_data = wpuc_metrics[wpuc_converter_type];
+        var wpuc_converter_base_unit = wpuc_converter_data['base_unit'];
+        var wpuc_converter_to = wpuc_converter_data['convert_to'];
+        var wpuc_converted_value;
+
+        if (wpuc_converter_type != 'temperature') {
+
+          wpuc_converted_value = wpuc_value * wpuc_converter_to[wpuc_from] * wpuc_converter_to[wpuc_converter_base_unit] / wpuc_converter_to[wpuc_to];
+    
+        } else {
+    
+          wpuc_converted_value = ((wpuc_value - wpuc_converter_to[wpuc_from]['base']) / wpuc_converter_to[wpuc_from]['ratio']) * wpuc_converter_to[wpuc_to]['ratio'] + wpuc_converter_to[wpuc_to]['base'];
+              
+        }
+
+        function toFixed(num, fixed) {
+          var re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
+          return num.toString().match(re)[0];
+        }
+
+        $("#wpuc_convert_result").html( 'Result: ' + toFixed(wpuc_converted_value, 2) + ' ' + wpuc_to );
+        $("#wpuc_convert_result").fadeIn();
+      }
+
     });
 });
 
   /**
-   * Sends Ajax request of selected metrics type by the user. It displays the units of the selected metrics as response handler, recieved from server.
+   * Sends Ajax request of selected metrics type by the user. It displays the units of the selected metrics as response handler.
    */
 
   $(document).ready(function($) {           
     $("#converter-selection .wpuc-select").change(function() {
-        $.post(wpuc_ajax_obj.ajax_url, {
-           _ajax_nonce: wpuc_ajax_obj.nonce,
-            action: "wpuc_do_change",
-            converter_type: this.value
-        }, function(data) {
-          $("#wpuc-converter-type").html(data); //inserts server response
+      var _this = this.value;
+      $.ajax({
+        url: wpuc_ajax_obj.metrics_json,
+        type: "GET",
+        dataType: "json",
+        success: displayUnits,
+        error: function(xhr, status) {
+          console.log("There was an error. Request couldn't be completed.");
+        }
+      });
+
+      function displayUnits(data) {
+        var wpuc_metrics = data['metrics'];
+        var converter = _this;
+        var wpuc_converter_data = wpuc_metrics[converter];
+        var wpuc_convert_options_array = wpuc_converter_data['select_box'];
+        
+        var wpuc_convert_options;
+
+        $.each( wpuc_convert_options_array, function( key, value ) {
+          
+          wpuc_convert_options += '<option value="' + key + '">' + value + '</option>';
         });
+
+        var show = '';
+
+        show += '<div id="wpuc-converter-type">';
+
+        show += '<div class="wpuc-converter-form">' +
+
+            '<input type="hidden" name="wpuc_converter_type" value="' + converter + '" id="wpuc_converter_type"/>' +
+
+            '<div class="wpuc-form-table">' +
+
+            '<div class="wpuc-field">' +
+            '<div class="wpuc-field-key">Conversion value:</div>' +				
+            '<input  class="wpuc-field-value wpuc-input"  type="text" name="wpuc_value" value="" id="wpuc_value" />' +
+            '</div>' +
+
+            '<div class="wpuc-field">' +
+            '<div class="wpuc-field-key">Convert from:</div>' +
+            '<select class="wpuc-field-value wpuc-select" id="wpuc_from">' + wpuc_convert_options + '</select>' +
+            '</div>';
+
+        show += '<div class="wpuc-field">' +
+            '<div class="wpuc-field-key">Convert to:</div>' +
+            '<select class="wpuc-field-value wpuc-select" id="wpuc_to">' + wpuc_convert_options + '</select>' +
+            '</div>';
+
+        show += '<input type="button" name="convert" value="Convert" id="wpuc_convert"></input>' +
+
+            '<div id="wpuc_convert_result" class="wpuc-convert-result"></div>';
+
+        show += '';
+
+        $("#wpuc-converter-type").html(show);
+
+      }
     });
 });
 })(jQuery);
