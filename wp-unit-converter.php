c87e2fbe555e81325a698bfe28e214a6107ded77
Ihtisham Zahoor <ihtisham.zahoor@outlook.com>
2020-04-01 21:08:13 +0500

    Bug fix, tested with the latest build, updated version

--- wp-unit-converter.php
+++ wp-unit-converter.php
@@ -9,7 +9,7 @@
  * that starts the plugin.
  *
  * @link              centangle.com
- * @since             1.0.0
+ * @since             1.0.2
  * @package           Wp_Unit_Converter
  *
  * @wordpress-plugin
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'WP_UNIT_CONVERTER_VERSION', '1.0.0' );
+define( 'WP_UNIT_CONVERTER_VERSION', '1.0.2' );
 
 /**
  * The code that runs during plugin activation.
